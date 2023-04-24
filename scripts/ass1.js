cc8d4ecb02acf3d6db78e53a4790ae176ef5b511
laiba tanveer <shahlaiba152@gmail.com>
2023-04-24 11:34:17 +0500

    ass1 completed

---
+++ scripts/ass1.js
@@ -0,0 +1,87 @@
+// ................DAY 01............
+
+// ___chapter 1 (alerts)____
+
+// alert("Thanks for your Input!");
+// alert ("Thanks for your Input!");
+
+// ___chapter 2 (variables for strings)____
+
+// var name = "Mark";
+// var name = "Mark";
+// name = "Ace";
+// var nationality;
+// nationality = "U.S.";
+
+// var floogle = "Mark";
+// var name = "Mark";
+// var floogle = "Mark";
+
+// var lessonAuthor = "Mark";
+// var guyWhoKeepsSayingHisOwnName = "Mark";
+// var x = "Mark";
+
+// var Lastname = "Smith";
+// var cityOfOrigin = "New Orleans"
+// var aussiegreeting = "g'day";
+
+// var nickname = "Bob";
+
+// alert ("Thanks for your input!");
+
+// var thanks = "Thanks for your Input!";
+// alert("thanks");
+
+// ___chapter 3(variables for numbers)____
+
+// var weight = 150;
+
+// weight + 25
+
+// var originalNumber = 23;
+// var newNumber = originalNumber + 7;
+
+// var originalNumber = 23;
+// var numberToBeAdded = 7;
+// var newNumber = originalNumber + numberToBeAdded;
+
+// var originalNumber = 90;
+// originalNumber = originalNumber + 10;
+
+// var originalNumber = 23;
+// var newNumber = originalNumber + 7;
+
+// var originalNumber = 23;
+// var newNumber = originalNumber + 7;
+
+// var 1stPresident = "Washington"; not run 
+// var prezWhoCame1st = "Washington";
+
+// alert (144);
+
+// var caseQty = 144
+// alert(caseQty);
+
+
+// ___chapter 4(variable names legal and illegal)____
+
+// userResponse
+// userResponseTime
+// userResponseTimeLimit
+// response
+
+// ___chapter 5(math expressions:familiar operators)____
+
+var popularNumber = 4
+
+
+
+
+
+
+
+
+
+
+
+                                                  
