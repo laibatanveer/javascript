775fbd7e83927a695cd2171a514bc574b842e83d
laiba tanveer <shahlaiba152@gmail.com>
2023-04-27 19:56:56 +0500

    ass2 complete

--- scripts/ass1.js
+++ scripts/ass1.js
@@ -72,16 +72,243 @@
 
 // ___chapter 5(math expressions:familiar operators)____
 
-var popularNumber = 4
+// var popularNumber = 4;
+
+// var popularNumber = 2 + 2;
+
+// alert(2+2);
+
+// var popularNumber = 12 - 24;
+//  var popularNumber = 3 * 12;
+
+//  var num = 10;
+//  var popularNumber = num + 200 ;
+
+//  var num = 10;
+//  var anotherNum = 1;
+//  var popularNumber = num + anotherNum;
+
+//  var whatsLeftOver = 10 % 3;
+
+//  var whatsLeftOver = 9 % 3;
+
+// ___chapter 6(math expressions:unfamiliar operators)____
+
+// num++;
+// num = num + 1;
+
+// num--;
+
+// var num = 1;
+// var newNumber = num++;
+
+// var num = 1;
+// var newNumber = ++num;
+
+// var num = 1
+// var newNumber = num--;
+
+// var num = 1;
+// var newNumber = --num;
+
+// ___chapter 7(math expressions: eliminating ambiguity)____
+
+// var totalCost = 1 + 3 * 4;
+
+// var totalCost = 1 + (3 * 4);
+
+// var totalCost = (1 + 3) * 4;
+
+// var resultOfComputation = ((2 *4) * 4) + 2;
+
+// resultOfComputation = (2 * 4) * (4 + 2);
 
 
+// ___chapter 8(concatenating text strings)____
+
+// alert("Thanks for your input!");
+
+// var message = "Thanks for yout input!";
+// alert(message);
+
+// alert("Thanks," + userName + "!");
+
+// var message = "Thanks,";
+// var banger = "!";
+
+// alert(message + username + banger);
+
+// alert ("Thanks," + "Susan" + "!");
+
+// var message = "Thanks,";
+// var userName = "Susan";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+
+// alert ("2" + "2");
+
+// alert ("2 plus 2 equals" + 2 + 2);
 
 
+// ___chapter 9(prompts)____
+
+// var spec = prompt("Your species?", "human");
+
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+
+// var numberOfCats = prompt("How many cats?");
+// var tooManyCats = numberOfCats + 1;
 
 
+// ................DAY 02............
+
+// ___chapter 10(if statements)____
+
+// var x = prompt("Where does the Pope live?");
+// if (x === "Vatican") {
+//    alert("Correct!");
+// }
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+//    alert("Correct!");
+// }
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+//    score++;
+//    userIQ = "genius";
+//    alert("Correct!");
+// }
+
+// ___chapter 11(comparison operators)____
+
+// if (fullName === "Mark" + " " + "Myers") {
+//     if (fullName === firstName + " " + "Myers") {
+//     if (fullName === firstName + " " + "Myers") {
+//     if (fullName === "firstName + " " + lastName) {
+//     if (totalCost === 81.50 + 135) 
+//     if (totalCost === materialsCost + 135) {
+//     if (totalCost === materialsCost + laborCost) {
+//     if (x + y === a - b) {
+//     if (yourTicketNumber !== 487208) {
+//     alert("Better luck next time.");
+//      }
+//     if (1 > 0) {
+//     if (0 < 1) {
+//     if (1 >= 0) {
+//     40
+//     if (1 >= 1) {
+//     if (0 <= 1) {
+//     if (1 <= 1) {
 
 
-
-
-
+// ___chapter 12(if else & else if statements)____
                                                   
+// var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//   alert("Correct!");
+//  var x = prompt("Where does the Pope live?");
+//     if (x === "Vatican") {
+// alert("Correct!");
+//  }
+//  if (x !== "Vatican") {
+//       alert("Wrong answer");
+// }
+//  if (x === "Vatican") {
+//   alert("Correct!");
+//  }
+//  else {
+//      alert("Wrong answer");
+//  }
+//  var correctAnswer = "Vatican";
+//  if (x === correctAnswer) {
+//     alert("Correct!");
+// }
+//  else {
+//     score--;
+//     userIQ = "problematic";
+//     alert("Incorrect");
+//  }
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+//     43
+//     alert("Correct!");
+//  }
+//  else if (x === "Rome") {
+//      alert("Incorrect but close");
+//  }
+//  else {
+//      alert("Incorrect");
+//  }
+//  var correctAnswer = "Vatican";
+//  if (x === correctAnswer) {
+// 43
+//  alert("Correct!");
+//  }
+//  else if (x === "Rome") {
+// alert("Incorrect but close");
+//  }
+// else {
+// alert("Incorrect");
+// }
+
+// ___chapter 13(testing sets of conditions)____
+
+// if (weight > 300 && time < 6) {
+//     alert("Come to our tryout!");
+//      }
+//      else {
+//      alert("Come to our cookout!");
+//       }
+//      if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//      alert("Come to our tryout!");
+//      }
+//      else {
+//       alert("Come to our cookout!");
+//       }
+//      if (SAT > avg || GPA > 2.5 || sport === "football") {
+//      alert("Welcome to Bubba State!");
+//      }
+//      else {
+//     alert("Have you looked into appliance repair?"); }
+//     if (age > 65 || age < 21 && res === "U.S.") {
+//     if ((age > 65 || age < 21) && res === "U.S.") {
+//       if (age > 65 || (age < 21 && res === "U.S.")) {
+
+// ___chapter 14(if statements nested)____
+
+// if ((x === y || a === b) && c === d) {
+//     g = h;
+//    }
+//     else {
+//     e = f;
+//    if (c === d) {
+//    if (x === y) {
+//      g = h;
+//    }
+//    else if (a === b) {
+//    g = h;
+//    }
+//    else {
+//    e = f;
+//    }
+//   }
+//   else {
+//    e = f;
+//    }
+                
+
+// ___chapter 15(arrays)____
+
+// var city0 = "Atlanta";
+//  var city1 = "Baltimore";
+//  var city2 = "Chicago";
+//  var city3 = "Denver";
+//  var city4 = "Los Angeles";
+// var city5 = "Seattle";
+//   alert("Welcome to " + city3);
+//   var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+//    alert("Welcome to " + cities[3]);
+//   var mixedArray = [1, "Bob", "Now is", true];
---
+++ scripts/ass2.js
@@ -0,0 +1,221 @@
+// .....................TASK 01..................
+
+// Calculate the zakat value, first, create a variable named "zakatPercentage" and
+// store the value of 2.5% in it (hint: 2.5% means 0.025). Next, create another variable
+// named "userInput" and take the input from the user using the prompt. Make sure
+// the input value is of a type number by converting the input string to a number
+// using a suitable method. Then, create a variable named "result" and assign its value
+// to the multiplication of the zakat percentage and user input. Finally, use an alert
+// message to display the calculated zakat value. The message should look like this:
+// "Your zakat value is xxx".
+
+// alert("Calculate your Zakat value");
+// var zakatPercentage = 0.025;
+// var userInput = +prompt("input");
+// var result = zakatPercentage * userInput;
+
+// alert("Your Zakat value is " + result);
+
+
+// // ...............................TASK 02 ..............................
+
+// // calculate the fitrah amount, first, ask the user to enter the total number of family
+// // members using the prompt and store the value in a variable called
+// // "familyMembers". Next, ask the user to choose a fitrah method by providing them
+// // options using the prompt, and store the selected method and its price in variables.
+// // Then, use an if-else block to check the user's input and calculate the fitrah amount
+// // by multiplying the selected method's price with the number of family members.
+// // Finally, display the calculated fitrah amount using an alert message.
+
+// alert("Calculate the Fitrah Amount");
+
+// var familyMembers = +prompt("Total number of your family members")
+
+// var wheat = 320;
+// var dates = 2800;
+// var raisin = 4800;
+// var oats = 480;
+// var method = prompt("wheat, dates, oats, raisin");
+// if
+//    (method = wheat) {
+//     var result = wheat * familyMembers
+//     alert("your fitrah value is " + result)
+
+//    }
+// else if(method =dates) 
+// {
+//     var result = dates * familyMembers
+//     alert("your fitrah value is " + result)
+// }
+// else if (method = oats)
+// {
+//     var result = oats * familyMembers
+//     alert("your fitrah value is " + result)
+// }
+
+// else if (method = raisin) {
+//     var result = raisin * familyMembers
+//     alert("your fitrah value is " + result)
+// }
+    
+
+// ........................TASK 03...........................
+
+// Create a program that generates a random number between 1 and 10 and stores it
+// in a variable called "secretNumber". Then, ask the user to enter a guess for the
+// secret number using a prompt.
+// Use an if-else statement to check if the user's guess matches the secret number. If
+// the guess is correct, display a message using an alert saying "Congratulations! You
+// guessed the secret number". Otherwise, if the guess is too high or too low, display
+// an appropriate message informing the user to guess again.
+
+// alert("Guess the secret number");
+
+// var secretNumber = 5;
+
+// var guessIt = +prompt("enter your guess number");
+
+// if(guessIt == 5){
+//     alert("Congratulations! You've guessed it correctly");
+
+// }
+// else if(guessIt>5)
+// {
+//     alert("Your guess is too high, try again");
+// }
+// else if(guessIt<5){
+//     alert("Your guess is too low, try again");
+// }
+
+// else {
+//     alert("Try again")}
+
+
+// ...............TASK 04.....................
+
+// Create a program that asks the user to enter a name, and then prints out the name
+// with the first letter capitalized (Make your name in capitalized case).
+
+// var nam = prompt("Enter your Name here");
+
+// var capitalizedname = nam[0].toUpperCase() + nam.slice(1).toLowerCase();
+
+// console.log(capitalizedname);
+
+// alert(capitalizedname);
+
+// ......................TASK 05.......................
+
+// In this task, you will be creating two empty arrays called "contactNumbers" and
+// "contactNames". Using the prompt, you will ask the user to enter a contact number
+// and contact name. You will then push these values into their respective arrays
+// using the push method. After adding all the contacts, you will display the contact
+// numbers and names in the console. To do this, you will need to use a for loop to
+// iterate through both arrays and log each element to the console.
+// Make sure to use descriptive variable names and comment on your code for clarity.
+
+// contactNames=[];
+// contactNumbers=[];
+
+// for( i=0 ; i<4 ; i++ ) {
+//   var personName =prompt("Enter Your Name");
+//   contactNames.push(personName);
+
+
+//   var personNumber =+prompt("Enter Your Contact number");
+//   contactNumbers.push(personNumber);
+
+// }
+
+// console.log("contactNames:   contactNumbers");
+
+// for( i=0 ; i<4 ; i++) {
+
+//   console.log(contactNames[i] + " : "  + contactNumbers[i]);
+// }
+
+// .........................TASK 06......................
+
+// Create an Array that contains different products, and get input from the user in which you
+// ask your user to give the position of that element he/she wants. Now remove that Item
+// from your array and console the removed item, Also display the remaining items in the
+// array and total number of items remaining.
+
+// var outCome=["pikachu","lugia","mew","snorlax"];
+
+// var userOutCome=+prompt("put the number of outcome you want to remove= \n 1.pikachu, \n 2.lugia, \n 3.mew, \n 4.snorlax");
+
+// var removedOutCome= outCome.splice(userOutCome,1);
+
+// console.log("The removed outcome is:" +removedOutCome);
+// alert("The removed outcome is:" +removedOutCome);
+
+
+// console.log("The remainder is");
+// for( i=0 ; i<outCome.length ; i+=2) {
+//  console.log (i +" " + outCome[i]);
+// }
+// console.log("the remainder is" + outCome.length);
+
+// .........................TASK 07......................
+
+// Create a program that asks the user for their nationality, gender, and age using the prompt
+// function. The program should then use nested if-else statements to determine if the
+// person is eligible to vote.
+// First, the program should check if the person is Pakistani or Indian. If they are not, the
+// program should display a message saying they are not eligible to vote.
+// If they are Pakistani or Indian, the program should then check their gender. If the person is
+// male and over the age of 18, they are eligible to vote. If the person is female and over the
+// age of 18, the program should ask if they are married. If they are married, they are eligible
+// to vote. If they are not married, they are not eligible to vote.
+// If the person is under 18, the program should display a message saying they are not eligible
+// to vote.
+
+// var Nationality = prompt("Enter Your Nationality");
+
+// var Gender = prompt ("Enter Your Gender");
+
+// var Age = +prompt ("Enter Your Age");
+
+// var maritalstatus = prompt("what is your marital status  \ 1. married \ 2. unmarried");
+
+
+// if (Nationality == "Pakistani" || "Indian") {
+
+//   alert("You are not eligible to vote");
+
+// } 
+// if(( Gender == 'Male' && Age>18)){
+// }
+
+// if( Gender== 'female' && Age>18){
+
+//   if(maritalstatus == 'married') {
+
+//    alert("You are eligible to vote")
+// }
+
+// else if (maritalstatus == 'unmarried'){
+
+//   alert("You are not eligible to  vote")
+// }
+// }
+
+// .........................TASK 08......................
+
+// You have an array that contains the names of Pakistani Team Players selected for
+// WorldCup (15 Players) named as WorldCupSquad. Now, tomorrow we have a match with
+// India, So make an array of final team players (11 Players) that will be playing in tomorrow's
+// match from the WorldCupSquad array.
+// (Hint : Make sure it should not disturb the array that contains 15 players instead you have
+// to make a copy of this array).
+
+// var WorldSquad = ["Babar Azam", "Asif Ali", "Haider Ali", "Shaheen Shah Afridi", "Haris Rauf", "Shadab Khan", "Khushdil Shah", "Mohammad Hasnain", "Iftikhar Ahmed", "Mohammad Nawaz", "Mohammad Rizwan", "Mohammad Wasim", "Naseem Shah", "Shan Masood", "Usman Qadir"];
+
+// var finalteam = WorldSquad.slice(0, 11);
+
+// console.log("The final team is " + finalteam);
+
+// alert("The final team is" + finalteam);
+
+// --------------------------------------------------------------------------------------------------------------------------------------
